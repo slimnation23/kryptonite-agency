99ca7a69f9a840cbf67db186798f7da99feb0c36
slimnation23 <55851245+slimnation23@users.noreply.github.com>
2023-03-31 10:41:42 +0200

    form done

--- script.js
+++ script.js
@@ -21,3 +21,45 @@ for (let anchor of anchors) {
     })
   })
 }
+
+//Form
+const popup = document.querySelector('.popup__wrapper')
+popup.addEventListener('click', () => (popup.style.display = 'none'))
+
+$(document).ready(function () {
+  const form = document.querySelector('#feedback')
+  const formSubmit = document.querySelector('#feedback__submit')
+
+  formSubmit.addEventListener('click', (e) => {
+    e.preventDefault()
+    const formName = document.querySelector('#feedback__name').value
+    const formTelegram = document.querySelector('#feedback__telegram').value
+    const formWebsite = document.querySelector('#feedback__website').value
+    const formEmail = document.querySelector('#feedback__email').value
+    const formSubject = document.querySelector('#feedback__subject').value
+    const formBudget = document.querySelector('#feedback__budget').value
+    const formMessage = document.querySelector('#feedback__message').value
+
+    $.ajax({
+      type: 'POST',
+      url: 'post.php',
+      data:
+        'name=' + formName +
+        '&telegram=' + formTelegram +
+        '&website=' + formWebsite +
+        '&email=' + formEmail +
+        '&subject=' + formSubject +
+        '&budget=' + formBudget +
+        '&mes=' + formMessage,
+      success: function () {
+        console.log('success!')
+      },
+      error: function (err) {
+        console.log('error')
+      },
+    })
+    popup.style.display = 'block'
+
+    form.reset()
+  })
+})
